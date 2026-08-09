9e58a2a04051705ae6bd90b62c79b6f8261b1d24
noah <130174758+nokway@users.noreply.github.com>
2026-08-09 21:50:31 +0200

    <feat>(files): Add close file might remove

--- lib/io_creator.rb
+++ lib/io_creator.rb
@@ -5,9 +5,8 @@ class AcessFile
     data_file = '~/Downloads/google-10000-english-no-swears.txt'
     @file = File.open(File.expand_path(data_file), 'r')
   end
-  
 
+  def close_file
+    file.close
+  end
 end
-
-file_obj = AcessFile.new
-p file_obj.file.readlines
